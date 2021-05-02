fbec92b31110ad75fb03940caa27e7453d121e2e
y-naga0926 <75489306+y-naga0926@users.noreply.github.com>
2021-05-02 11:14:04 +0900

    newアクション・createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
-  # def new
-    # @user = User.new
-  # end
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to new_item_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :discription, :user, :image).merge(user_id: current_user.id)
+  end
 end
