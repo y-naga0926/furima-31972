a41501272b4a59db4b1eb3a7d4f5822842641f5c
y-naga0926 <75489306+y-naga0926@users.noreply.github.com>
2021-05-10 20:46:23 +0900

    売却済み購入ページには遷移できない設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,6 +7,9 @@ class PurchasesController < ApplicationController
     if current_user == @item.user
       redirect_to root_path
     end
+    if @item.purchase.present?
+      redirect_to root_path
+    end
   end
 
   def create
