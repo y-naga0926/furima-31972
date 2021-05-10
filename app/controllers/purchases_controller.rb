3212c8eeb94cb78f248773a0005988a7984ddb6d
y-naga0926 <75489306+y-naga0926@users.noreply.github.com>
2021-05-10 20:35:17 +0900

    直接URLを売ってページを遷移しない実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :move_to_signed_in, expect: [:index]
   def index
     @item = Item.find(params[:item_id])
